6812dd4c34e8444e522906d742eec00c291f641c
babboeCodes <babboecodes@gmail.com>
2022-10-16 08:47:39 +0100

    create component

---
+++ src/assets/data/example-nfts.js
@@ -0,0 +1,44 @@
+const data = [
+   {
+      image: require('../images/nft1.png'),
+      name: 'Korode',
+      rank: 300,
+      category: 'Epic',
+      other_data: '4446/5000',
+      can_play: false,
+   },
+   {
+      image: require('../images/nft2.png'),
+      name: 'Korode',
+      rank: 300,
+      category: 'Epic',
+      other_data: '4446/5000',
+      can_play: false,
+   },
+   {
+      image: require('../images/nft1.png'),
+      name: 'Anis',
+      rank: 700,
+      category: 'Legendary',
+      other_data: '4436/5000',
+      can_play: true,
+   },
+   {
+      image: require('../images/nft2.png'),
+      name: 'Kassem',
+      rank: 400,
+      category: 'Gold',
+      other_data: '146/5000',
+      can_play: true,
+   },
+   {
+      image: require('../images/nft1.png'),
+      name: 'Anis',
+      rank: 700,
+      category: 'Legendary',
+      other_data: '4436/5000',
+      can_play: true,
+   },
+];
+
+export default data;
