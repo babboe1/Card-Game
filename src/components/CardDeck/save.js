1eaf2fffdef5e82cb396bf7923ad7a56741c29e7
babboeCodes <babboecodes@gmail.com>
2022-10-27 09:13:33 +0100

    create save file

---
+++ src/components/CardDeck/save.js
@@ -0,0 +1,35 @@
+
+// CARDDECK
+
+
+// import React from 'react';
+// import Card from '../CardDraw/Card/Card';
+// import './CardDeck.css';
+
+// const CardDeck = (props) => {
+//    const animateDeck = props.deck.map((card, idx) => {
+//       return (
+//          <Card
+//             key={idx}
+//             id={idx}
+//             offSet={idx * 20}
+//             index={idx}
+//             top={50}
+//             translate="translate(-50%, -50%)"
+//             delay={idx}
+//          />
+//       );
+//    });
+
+//    const renderDeck = props.deck.map((nft, idx) => {
+//       return <Card key={idx} offSet={20 * idx} index={-idx} />;
+//    });
+
+//    return (
+//       <div className="out-deck__content">
+//          {props.flag ? animateDeck : renderDeck}
+//       </div>
+//    );
+// };
+
+// export default CardDeck;
